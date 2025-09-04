78d80a953a8aa4024dbadf6dd72349d956ff4b84
Pace Bailey <82382419+pacesbailey@users.noreply.github.com>
2025-09-04 11:49:39 +0200

    psycopg2 rework

--- scripts/db_converter.py
+++ scripts/db_converter.py
@@ -1,63 +1,53 @@
-"""This script converts a SQLite database to a JSONL file."""
+import json
+import os
 
 import argparse
-import json
-import sqlite3
-
-from queries import Queries, QueryTypes
+import psycopg2
+from dotenv import load_dotenv
 
 
-def query_db(cursor: sqlite3.Cursor, query_type: str) -> sqlite3.Cursor:
+load_dotenv()
+
+FILEPATH: str = "results.jsonl"
+QUERY: str = """select c.zotitem_ptr_id, c.status,  c.bibtex_author, c.bibtex_title, c.bibtex_year, c.bibtex_type 
+from core_zotaddon c
+where c.status = 'abgeschlossen' 
+order by zotitem_ptr_id asc, bibtex_author asc
+limit 10;"""
+
+
+def to_jsonl(results: list[tuple], filepath: str) -> None:
     """
-    Queries the database and returns the results.
+    Convert a list of tuples to a JSONL file.
 
     Args:
-        cursor (sqlite3.Cursor): the cursor to use to query the database
-        query_type (str): the type of query to execute
-
-    Returns:
-        sqlite3.Cursor: the results of the query
-
-    Raises:
-        ValueError: if the specified query type is invalid
+        results: A list of tuples to convert to JSONL.
+        filepath: The path to the JSONL file to write.
     """
-    match query_type:
-        case QueryTypes.SINGLE.value:
-            return cursor.execute(Queries.SINGLE.value)
-        case _:
-            raise ValueError(f"Invalid query type: {query_type}")
-
-
-def results_to_json(results: sqlite3.Cursor, jsonl_path: str) -> None:
-    """
-    Writes the query results to a JSONL file.
-
-    Args:
-        results (sqlite3.Cursor): the results of the query
-        jsonl_path (str): the path to the JSONL file
-    """
-    with open(jsonl_path, "w") as f:
-        for row in results:
-            f.write(f'{json.dumps(row)}\n')
+    with open(filepath, "w", encoding="utf-8") as f:
+        for result in results:
+            f.write(json.dumps(result) + "\n")
 
 
 def main(args: argparse.Namespace) -> None:
-    """
-    Connects to a database, queries it, then writes the results to a JSONL file.
-
-    Args:
-        args (argparse.Namespace): the arguments
-    """
-    connection: sqlite3.Connection = sqlite3.connect(args.db_path)
-    cursor: sqlite3.Cursor = connection.cursor()
-    results: sqlite3.Cursor = query_db(cursor, args.query_type)
-    results_to_json(results, args.jsonl_path)
+    with psycopg2.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        port=os.getenv("DB_PORT", "5432"),
+        database=os.getenv("DB_NAME", "kibad"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    ) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(args.query)
+            results: list[tuple] = cursor.fetchall()
+            print(results)
+            # to_jsonl(results, args.filepath)
 
 
 if __name__ == "__main__":
     parser: argparse.ArgumentParser = argparse.ArgumentParser()
-    parser.add_argument("--db_path", type=str, required=True)
-    parser.add_argument("--jsonl_path", type=str, required=True)
-    parser.add_argument("--query_type", type=str, default="single", choices=QueryTypes.list())
+    parser.add_argument("--filepath", type=str, default=FILEPATH)
+    parser.add_argument("--query", type=str, default=QUERY)
+    
     args: argparse.Namespace = parser.parse_args()
     main(args)
