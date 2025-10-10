4f3fd83964c936bdd5c92966055cfd429accb0a4
Arne Binder <arne.binder@dfki.de>
2025-10-10 19:38:26 +0200

    fix paths in test configs

--- tests/integration/test_predict.py
+++ tests/integration/test_predict.py
@@ -3,14 +3,14 @@ from omegaconf import open_dict
 import pytest
 
 from kibad_llm.config import PROJ_ROOT
-from kibad_llm.predict import predict_single, predict
+from kibad_llm.predict import predict, predict_single
 
 
 @pytest.mark.slow
 def test_predict_single(tmp_path, cfg_predict):
 
     with open_dict(cfg_predict):
-        cfg_predict.pdf_file = PROJ_ROOT / "tests" / "fixtures" / "pdfs" / "7T8NZA5Q.pdf"
+        cfg_predict.pdf_file = str(PROJ_ROOT / "tests" / "fixtures" / "pdfs" / "7T8NZA5Q.pdf")
 
     HydraConfig().set_config(cfg_predict)
     predict_single(cfg_predict)
@@ -20,7 +20,7 @@ def test_predict_single(tmp_path, cfg_predict):
 def test_predict(tmp_path, cfg_predict):
 
     with open_dict(cfg_predict):
-        cfg_predict.pdf_directory = PROJ_ROOT / "tests" / "fixtures" / "pdfs"
+        cfg_predict.pdf_directory = str(PROJ_ROOT / "tests" / "fixtures" / "pdfs")
 
     HydraConfig().set_config(cfg_predict)
     predict(cfg_predict)
@@ -30,7 +30,7 @@ def test_predict(tmp_path, cfg_predict):
 def test_predict_fast_dev_run(tmp_path, cfg_predict):
 
     with open_dict(cfg_predict):
-        cfg_predict.pdf_directory = PROJ_ROOT / "tests" / "fixtures" / "pdfs"
+        cfg_predict.pdf_directory = str(PROJ_ROOT / "tests" / "fixtures" / "pdfs")
         cfg_predict.fast_dev_run = True
 
     HydraConfig().set_config(cfg_predict)
