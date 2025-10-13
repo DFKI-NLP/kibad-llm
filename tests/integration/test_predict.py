eb2b081ab3502c4dc1a8621d7bc51ca3a7b51382
Arne Binder <arne.binder@dfki.de>
2025-10-13 18:17:16 +0200

    check structured result content

--- tests/integration/test_predict.py
+++ tests/integration/test_predict.py
@@ -17,6 +17,26 @@ def test_predict(tmp_path, cfg_predict):
     HydraConfig().set_config(cfg_predict)
     predict(cfg_predict)
 
+    # read json line file from cfg_predict.output_file
+    results = []
+    with open(cfg_predict.output_file) as f:
+        lines = f.readlines()
+        for line in lines:
+            results.append(json.loads(line))
+
+    for result in results:
+        fixture_path = PROJ_ROOT / "tests" / "fixtures" / "results" / f"{result['file_name']}.json"
+
+        # write fixture data
+        # with open(fixture_path, "w") as f:
+        #    json.dump(result, f, indent=4)
+
+        # read fixture data
+        with open(fixture_path) as f:
+            result_expected = json.load(f)
+
+        assert result["structured"] == result_expected["structured"]
+
 
 @pytest.mark.slow
 def test_predict_fast_dev_run(tmp_path, cfg_predict):
@@ -42,9 +62,9 @@ def test_predict_fast_dev_run(tmp_path, cfg_predict):
 
     # write fixture data
     # with open(fixture_path, "w") as f:
-    #    json.dump(result, f, indent=4)
+    #   json.dump(result, f, indent=4)
 
     # read fixture data
     with open(fixture_path) as f:
         fixture_data = json.load(f)
-    assert result == fixture_data
+    assert result["structured"] == fixture_data["structured"]
