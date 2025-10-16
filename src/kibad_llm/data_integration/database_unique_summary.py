10646a187a4d5bb380dd6889623d78f878326508
harbecke <26972154+harbecke@users.noreply.github.com>
2025-10-16 14:59:06 +0200

    beautify, add docstring, pass checks

--- src/kibad_llm/data_integration/database_unique_summary.py
+++ src/kibad_llm/data_integration/database_unique_summary.py
@@ -1,31 +1,70 @@
 import pandas as pd
 
-df = pd.read_json("data/interim/faktencheck-db/faktencheck-db-converted_2025-08-19.jsonl", lines=True)
 
-str_cols = ['zotitem_ptr_id', 'status', 'bibtex_author', 'bibtex_title', 'bibtex_year', 'bibtex_type']
-list_cols = [
-    'biodiversity_level', 'biodiversity_variable', 'climate', 'habitat', 'landuse', 'measure', 'method',
-    'natural_region', 'study_type', 'transformation_potential'
-]
-list_of_dict_cols = [
-    'conservation_area', 'direct_driver', 'ecosystem_service', 'ecosystem_type',  'impulse_measure', 'indirect_driver',
-    'location', 'management_measure', 'soil', 'taxa'
-]
+def main(data_path: str) -> dict[str, dict]:
+    """
+    Reads the jsonl converted Faktencheck database and extracts unique values.
+    Some columns have nested entries, e.g. "direct_driver", these are a list of a dict.
+    The database is flattened and the column name combined with the key to generate uniques for the values.
+    Should the database or conversion change the columns, they need to be changed manually.
 
-unique_dict = {}
+    :param data_path: path to Faktencheck database conversion (.jsonl)
+    :return: dictionary with unique values for each (sub-)column
+    """
+    df = pd.read_json(data_path, lines=True)
 
-for col in str_cols:
-    unique_dict[col] = df[col].unique().tolist()
+    str_cols = [
+        "zotitem_ptr_id",
+        "status",
+        "bibtex_author",
+        "bibtex_title",
+        "bibtex_year",
+        "bibtex_type",
+    ]
+    list_cols = [
+        "biodiversity_level",
+        "biodiversity_variable",
+        "climate",
+        "habitat",
+        "landuse",
+        "measure",
+        "method",
+        "natural_region",
+        "study_type",
+        "transformation_potential",
+    ]
+    list_of_dict_cols = [
+        "conservation_area",
+        "direct_driver",
+        "ecosystem_service",
+        "ecosystem_type",
+        "impulse_measure",
+        "indirect_driver",
+        "location",
+        "management_measure",
+        "soil",
+        "taxa",
+    ]
 
-for col in list_cols:
-    unique_dict[col] = df[col].explode().unique().tolist()
+    unique_dict = {}
 
-for col in list_of_dict_cols:
-    normalized_df = pd.json_normalize(df[col].explode())
-    normalized_df = normalized_df.replace({float('nan'): None})
-    sub_cols = normalized_df.columns.to_list()
-    for sub_col in sub_cols:
-        unique_dict[f"{col}.{sub_col}"] = normalized_df[sub_col].unique().tolist()
+    for col in str_cols:
+        unique_dict[col] = df[col].unique().tolist()
 
-unique_dict_len = {dict_name: len(dict_list) for dict_name, dict_list in unique_dict.items()}
-print(unique_dict_len)
+    for col in list_cols:
+        unique_dict[col] = df[col].explode().unique().tolist()
+
+    for col in list_of_dict_cols:
+        normalized_df = pd.json_normalize(df[col].explode())  # type: ignore
+        normalized_df = normalized_df.replace({float("nan"): None})
+        sub_cols = normalized_df.columns.to_list()
+        for sub_col in sub_cols:
+            unique_dict[f"{col}.{sub_col}"] = normalized_df[sub_col].unique().tolist()
+
+    return unique_dict
+
+
+if __name__ == "__main__":
+    unique_dict = main("data/interim/faktencheck-db/faktencheck-db-converted_2025-08-19.jsonl")
+    unique_dict_len = {dict_name: len(dict_list) for dict_name, dict_list in unique_dict.items()}
+    print(unique_dict_len)
