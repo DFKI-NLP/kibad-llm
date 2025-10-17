109a91d1a6d5e58b1a1b753f5c621ab89cb8c395
Arne Binder <arne.binder@dfki.de>
2025-10-17 17:41:43 +0200

    minor improvements

--- src/kibad_llm/preprocessing.py
+++ src/kibad_llm/preprocessing.py
@@ -15,6 +15,6 @@ def read_pdf_as_markdown(file_name: str, base_path: Path) -> dict[str, str]:
         file_name: The name of the PDF file to read.
         base_path: The base path where the PDF file is located.
     Returns:
-        A dictionary with a single key "text" containing the markdown text.
+        A dictionary with a single key "text" containing the Markdown text.
     """
     return {"text": pymupdf4llm.to_markdown(str(base_path / file_name))}
--- tests/unit/test_preprocessing.py
+++ tests/unit/test_preprocessing.py
@@ -15,13 +15,13 @@ def test_read_pdf_as_markdown():
     assert set(result) == {"text"}
     markdown = result["text"]
 
-    # get expected markdown from fixture
     markdown_path = PROJ_ROOT / "tests" / "fixtures" / "markdown" / f"{file_name}.json"
 
     # write fixture data
     # with open(markdown_path, "w") as f:
     #      json.dump({"text": markdown}, f, indent=4, ensure_ascii=False)
 
+    # get expected markdown from fixture
     with open(markdown_path) as f:
         markdown_data = json.load(f)
     markdown_expected = markdown_data["text"]
