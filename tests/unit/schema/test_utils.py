44aba60229557106887cc56e448fb2ab9bc06357
Arne Binder <arne.binder@dfki.de>
2025-10-21 12:48:37 +0200

    adjust test_build_schema_description for removed "UNBEKANNT" and added cardinality

--- tests/unit/schema/test_utils.py
+++ tests/unit/schema/test_utils.py
@@ -9,11 +9,11 @@ def test_build_schema_description():
     expected = inspect.cleandoc(
         """
         Beschreibung: Angaben zu den ökosystembezogenen Studienmerkmalen.
-        Feldhinweise und erlaubte Werte:
-        - Lebensräume: In welchen Lebensräumen wurde die Studie durchgeführt? Zulässige Werte: Wald; Agrar- und Offenland; Binnengewässer und Auen; Küste und Küstengewässer; Urbane Räume; Boden
-        - Naturgroßräume: In welchen Naturgroßräumen wurde die Studie durchgeführt? Zulässige Werte: Alpen; Alpenvorland; Mittelgebirgsschwelle; Norddeutsches Tiefland; Nord- und Ostsee oder Schichtsstufenland beidseits des Oberrheingrabens
-        - Ökosystemtyp: Welchen Ökosystemtyp hat die Studie betrachtet? Zulässige Werte: Binnenland: Waldfreie Niedermoore und Sümpfe, Grünland nasser bis feuchter Standorte; Binnenland: Laub(Misch)Wälder und -Forste (Laubbaumanteil über 50 Prozent); Binnengewässer: Stehende Gewässer; Meere und Küsten: Benthal der Nordsee; UNBEKANNT
-        - Bundesland: In welchem Bundesland liegt das betrachtete Ökosystem der Studie? Zulässige Werte: Baden-Württemberg; Bayern; Berlin; Brandenburg; Bremen; Hamburg; Hessen; Mecklenburg-Vorpommern; Niedersachsen; Nordrhein-Westfalen; Rheinland-Pfalz; Saarland; Sachsen; Sachsen-Anhalt; Schleswig-Holstein; Thüringen; UNBEKANNT
+        Feldhinweise und erlaubte Werte (getrennt durch Semikolons):
+        - Lebensräume: In welchen Lebensräumen wurde die Studie durchgeführt? Kardinalität: 0..* | Zulässige Werte: Wald; Agrar- und Offenland; Binnengewässer und Auen; Küste und Küstengewässer; Urbane Räume; Boden
+        - Naturgroßräume: In welchen Naturgroßräumen wurde die Studie durchgeführt? Kardinalität: 0..* | Zulässige Werte: Alpen; Alpenvorland; Mittelgebirgsschwelle; Norddeutsches Tiefland; Nord- und Ostsee oder Schichtsstufenland beidseits des Oberrheingrabens
+        - Ökosystemtyp: Welchen Ökosystemtyp hat die Studie betrachtet? Kardinalität: 0..1 | Zulässige Werte: Binnenland: Waldfreie Niedermoore und Sümpfe, Grünland nasser bis feuchter Standorte; Binnenland: Laub(Misch)Wälder und -Forste (Laubbaumanteil über 50 Prozent); Binnengewässer: Stehende Gewässer; Meere und Küsten: Benthal der Nordsee
+        - Bundesland: In welchem Bundesland liegt das betrachtete Ökosystem der Studie? Kardinalität: 0..1 | Zulässige Werte: Baden-Württemberg; Bayern; Berlin; Brandenburg; Bremen; Hamburg; Hessen; Mecklenburg-Vorpommern; Niedersachsen; Nordrhein-Westfalen; Rheinland-Pfalz; Saarland; Sachsen; Sachsen-Anhalt; Schleswig-Holstein; Thüringen
         """
     )
     assert description == expected
