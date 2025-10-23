bf5feaae08f6302d4fa18584fe3553d023dbb357
Arne Binder <arne.binder@dfki.de>
2025-10-23 14:53:21 +0200

    move flatten_dict_simple to utils.dictionary

--- src/kibad_llm/evaluate.py
+++ src/kibad_llm/evaluate.py
@@ -13,6 +13,7 @@ from omegaconf import DictConfig
 
 from kibad_llm.config import PROJ_ROOT
 from kibad_llm.metric import Metric
+from kibad_llm.utils.dictionary import flatten_dict_simple
 
 logger = logging.getLogger(__name__)
 
@@ -27,68 +28,26 @@ def _get_key_from_reference(entry: Mapping[str, Any]) -> str:
     return entry["zotitem_ptr_id"]
 
 
-def _get_and_add_reference(prediction: Mapping[str, Any], references: dict[str, dict]) -> dict:
-    """Get the corresponding reference for a prediction and flatten it. Return as dict
+def _get_reference(prediction: Mapping[str, Any], references: dict[str, dict]) -> dict:
+    """Get the corresponding reference for a prediction. Return as dict
     with single key to comply with datasets map function."""
     prediction_key = _get_key_from_prediction(prediction)
     return {"reference": references[prediction_key]}
 
 
-def flatten_dict_simple(d: Mapping[str, Any], sep: str = ".") -> dict[str, Any]:
-    """Flatten a dictionary with simple rules:
-    - Keep only non-empty primitive values (str, int, float, bool)
-    - For lists of primitives, keep as is
-    - For lists of dicts, create new keys by combining parent key and child keys,
-      and aggregate values into lists (removing duplicates and sorting)
-
-    IMPORTANT: This function does not handle nested dicts beyond one level inside lists.
-
-    Args:
-        d: The dictionary to flatten.
-        sep: The separator to use when combining keys.
-    Returns:
-        A flattened dictionary.
-    """
-    result: dict[str, Any] = dict()
-    for k, v in d.items():
-        # remove empty values
-        if v is None or (isinstance(v, str) and v.strip() == ""):
-            pass
-        elif isinstance(v, (str, int, float, bool)):
-            result[k] = v
-        elif isinstance(v, list):
-            if all(isinstance(e, (str, int, float, bool)) for e in v):
-                result[k] = v
-            elif all(isinstance(e, dict) for e in v):
-                current_keys = set()
-                for e in v:
-                    for k2, v2 in e.items():
-                        if v2 is not None and not (isinstance(v2, str) and v2.strip() == ""):
-                            (
-                                result[f"{k}{sep}{k2}"].append(v2)
-                                if f"{k}{sep}{k2}" in result
-                                else result.setdefault(f"{k}{sep}{k2}", [v2])
-                            )
-                            current_keys.add(f"{k}{sep}{k2}")
-                for k2 in current_keys:
-                    result[k2] = sorted(set(result[k2]))
-            else:
-                raise ValueError(f"Cannot flatten list with mixed types: {v}")
-
-    return result
-
-
 def _prepare_reference(reference: Mapping[str, Any]) -> dict[str, Any]:
+    """Prepare reference by flattening the dict structure. Return as dict
+    with single key to comply with datasets map function."""
     reference_flat = flatten_dict_simple(reference, sep="/")
     return {"reference": reference_flat}
 
 
 def _prepare_prediction(
-    entry: Mapping[str, Any], json_paths_mapping: Mapping[str, str]
+    prediction: Mapping[str, Any], json_paths_mapping: Mapping[str, str]
 ) -> dict[str, Any]:
     """Prepare prediction by taking only the structured part and mapping schema keys to
     json paths. Return as dict with single key to comply with datasets map function."""
-    mapped = {json_paths_mapping[k]: v for k, v in entry["structured"].items()}
+    mapped = {json_paths_mapping[k]: v for k, v in prediction["structured"].items()}
     return {"prediction": mapped}
 
 
@@ -111,10 +70,8 @@ def evaluate(cfg: DictConfig) -> dict[str, Any]:
             entry = json.loads(line)
             references_dict[_get_key_from_reference(entry)] = entry
 
-    logger.info("Aligning predictions with references ...")
-    predictions = predictions.map(
-        _get_and_add_reference, fn_kwargs={"references": references_dict}
-    )
+    logger.info("Aligning references with predictions ...")
+    predictions = predictions.map(_get_reference, fn_kwargs={"references": references_dict})
     logger.info("Preparing references for metric computation ...")
     predictions = predictions.map(_prepare_reference, input_columns=["reference"])
     logger.info("Preparing predictions for metric computation (map schema keys to json paths) ...")
