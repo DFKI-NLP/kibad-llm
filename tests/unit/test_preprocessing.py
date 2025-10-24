0d31afcdfee01df09d33c3458dbcac393104b159
Arne Binder <arne.binder@dfki.de>
2025-10-24 16:08:38 +0200

    adjust test_read_pdf_as_markdown_via_pymupdf4llm

--- tests/unit/test_preprocessing.py
+++ tests/unit/test_preprocessing.py
@@ -11,19 +11,17 @@ def test_read_pdf_as_markdown_via_pymupdf4llm():
         file_name=file_name, base_path=PROJ_ROOT / "tests" / "fixtures" / "pdfs"
     )
 
-    assert isinstance(result, dict)
-    assert set(result) == {"text"}
-    markdown = result["text"]
+    assert isinstance(result, str)
 
     markdown_path = PROJ_ROOT / "tests" / "fixtures" / "markdown" / f"{file_name}.json"
 
     # write fixture data
     # with open(markdown_path, "w") as f:
-    #      json.dump({"text": markdown}, f, indent=4, ensure_ascii=False)
+    #      json.dump({"text": result}, f, indent=4, ensure_ascii=False)
 
     # get expected markdown from fixture
     with open(markdown_path) as f:
         markdown_data = json.load(f)
-    markdown_expected = markdown_data["text"]
+    result_expected = markdown_data["text"]
 
-    assert markdown == markdown_expected
+    assert result == result_expected
