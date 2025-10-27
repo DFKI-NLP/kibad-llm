2c1f10517fd80308f03f9131a3894bf713f5b1c1
Theoreticallyhugo <me@theoreticallyhugo.com>
2025-10-27 18:01:43 +0100

    improve docstrings

--- src/kibad_llm/metrics/f1.py
+++ src/kibad_llm/metrics/f1.py
@@ -5,6 +5,15 @@ from kibad_llm.metric import Metric
 
 class MicroF1Metric(Metric):
     """Computes precision, recall, and F1 score for single- and multi-label classification tasks.
+
+        If self.field is set, this class assumes the prediction and reference inputs to be dictionaries.
+        In that case it uses self.field to try and extract the relevant prediction and reference
+        values from the prediction and reference dictionaries.
+        If self.field is none, this class assumes the inputs to be a list, set, or single value.
+        The inputs are later converted into sets by self._prepare_entry.
+        Warning:
+        !This can obfuscate issues, like an llm returning the same value as prediction 100 times!
+
     Args:
         field: The optional field name in the predictions and references to evaluate.
     """
@@ -18,7 +27,16 @@ class MicroF1Metric(Metric):
         self.state: dict[str, int] = {"tp": 0, "fp": 0, "fn": 0}
 
     def _prepare_entry(self, entry: Any) -> set:
-        """Helper method to convert an entry into a set of values."""
+        """Helper method to convert any prediction or reference value into a set of values.
+
+        Uses self.field to retrieve the correct values from a given dict if necessary.
+        Returns empty set when there is no value.
+        Wraps any found values into a set whilst keeping the unique values unaltered
+
+        Args:
+            entry: Any kind of data structure to maybe extract from and eventually wrap in a set.
+        Returns: A set of whatever relevant value was put in.
+        """
         if self.field is not None:
             entry = entry.get(self.field, None)
         if entry is None:
@@ -29,14 +47,6 @@ class MicroF1Metric(Metric):
 
     def _update(self, prediction: Any, reference: Any) -> None:
         """Updates the internal state with the given prediction(s) and reference(s).
-
-        If self.field is set, this method assumes the input to be a dictionary.
-        In that case it uses self.field to try and extract the relevant prediction and reference
-        values from the prediction and reference dictionaries.
-        If self.field is none, this method assumes the inputs to be a list, set, or single value.
-        The inputs are later converted into sets.
-        Warning:
-        !This can obfuscate issues, like an llm returning the same value as prediction 100 times!
 
         Args:
             prediction: Dict of predicted value(s) or predicted values directly
