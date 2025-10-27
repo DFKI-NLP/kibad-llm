caa717072eccff86123f495275f9a6c07fd5d658
Arne Binder <arne.binder@dfki.de>
2025-10-27 18:45:13 +0100

    adjust adjust test_build_schema_description

--- tests/unit/schema/test_utils.py
+++ tests/unit/schema/test_utils.py
@@ -12,8 +12,8 @@ def test_build_schema_description():
         Feldhinweise und erlaubte Werte (getrennt durch Semikolons):
         - Lebensräume: In welchen Lebensräumen wurde die Studie durchgeführt? Kardinalität: 0..* | Zulässige Werte: Wald; Agrar- und Offenland; Binnengewässer und Auen; Küste und Küstengewässer; Urbane Räume; Boden
         - Naturgroßräume: In welchen Naturgroßräumen wurde die Studie durchgeführt? Kardinalität: 0..* | Zulässige Werte: Alpen; Alpenvorland; Mittelgebirgsschwelle; Norddeutsches Tiefland; Nord- und Ostsee oder Schichtsstufenland beidseits des Oberrheingrabens
-        - Ökosystemtyp: Welchen Ökosystemtyp hat die Studie betrachtet? Kardinalität: 0..1 | Zulässige Werte: Binnenland: Waldfreie Niedermoore und Sümpfe, Grünland nasser bis feuchter Standorte; Binnenland: Laub(Misch)Wälder und -Forste (Laubbaumanteil über 50 Prozent); Binnengewässer: Stehende Gewässer; Meere und Küsten: Benthal der Nordsee
-        - Bundesland: In welchem Bundesland liegt das betrachtete Ökosystem der Studie? Kardinalität: 0..1 | Zulässige Werte: Baden-Württemberg; Bayern; Berlin; Brandenburg; Bremen; Hamburg; Hessen; Mecklenburg-Vorpommern; Niedersachsen; Nordrhein-Westfalen; Rheinland-Pfalz; Saarland; Sachsen; Sachsen-Anhalt; Schleswig-Holstein; Thüringen
+        - Ökosystemtyp: Welche Ökosystemtypen hat die Studie betrachtet? Kardinalität: 0..* | Zulässige Werte: Binnenland: Waldfreie Niedermoore und Sümpfe, Grünland nasser bis feuchter Standorte; Binnenland: Laub(Misch)Wälder und -Forste (Laubbaumanteil über 50 Prozent); Binnengewässer: Stehende Gewässer; Meere und Küsten: Benthal der Nordsee
+        - Bundesland: In welchen Bundesländern liegt das betrachtete Ökosystem der Studie? Kardinalität: 0..* | Zulässige Werte: Baden-Württemberg; Bayern; Berlin; Brandenburg; Bremen; Hamburg; Hessen; Mecklenburg-Vorpommern; Niedersachsen; Nordrhein-Westfalen; Rheinland-Pfalz; Saarland; Sachsen; Sachsen-Anhalt; Schleswig-Holstein; Thüringen
         """
     )
     assert description == expected
