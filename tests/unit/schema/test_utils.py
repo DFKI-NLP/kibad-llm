cbe5dd0d32ea096085bc0b3f773f539c6a52cb3d
Arne Binder <arne.binder@dfki.de>
2025-11-01 00:20:15 +0100

    adjust schema description fixture

--- tests/unit/schema/test_utils.py
+++ tests/unit/schema/test_utils.py
@@ -1,13 +1,13 @@
 from kibad_llm.config import PROJ_ROOT
-from kibad_llm.schema.types import EcosystemStudyFeatures
+from kibad_llm.schema.types import EcosystemStudyFeaturesWithoutCompounds
 from kibad_llm.schema.utils import build_schema_description
 from tests.conftest import WRITE_FIXTURE_DATA
 
 
 def test_build_schema_description():
-    description = build_schema_description(EcosystemStudyFeatures.model_json_schema())
+    description = build_schema_description(EcosystemStudyFeaturesWithoutCompounds.model_json_schema())
     path_expected = (
-        PROJ_ROOT / "tests" / "fixtures" / "schema" / "description_ecosystem_study_features.txt"
+        PROJ_ROOT / "tests" / "fixtures" / "schema" / "default.txt"
     )
     if WRITE_FIXTURE_DATA:
         with open(path_expected, "w") as f:
