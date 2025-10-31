fb2e193ec2058996b2b5ca6ef5a7c6991d7970b5
Arne Binder <arne.binder@dfki.de>
2025-10-31 15:23:48 +0100

    add EcosystemTypeCategoryEnum

--- src/kibad_llm/schema/types.py
+++ src/kibad_llm/schema/types.py
@@ -40,6 +40,18 @@ class LocationFederalStateEnum(str, Enum):
     THUERINGEN = "Thüringen"
 
 
+# TODO: this is not yet used. What is the entry in the Fragenkatalog table for "ecosystem_type" -> "category"? I could not find it.
+class EcosystemTypeCategoryEnum(str, Enum):
+    I_MEERE_UND_KUESTEN = "I Biotoptypengruppen der Meere und Küsten"
+    II_BINNENGEWAESSER = "II Biotoptypengruppen der Binnengewässer"
+    III_BINNENLAND_TERRESTRISCH_SEMITERRESTRISCH = (
+        "III Terrestrische und semiterrestrische Biotoptypengruppen des Binnenlandes"
+    )
+    IV_TECHNISCHE = "IV Technische Biotoptypengruppen"
+    V_ALPEN = "V Biotoptypengruppen mit Schwerpunkt in den Alpen"
+    VI_WEITERE = "VI Weitere"
+
+
 class EcosystemTypeTermEnum(str, Enum):
     ANTHROPOGENE_ROHBODEN = "Anthropogene Rohbodenstandorte und Ruderalfluren"
     BAUWERKE = "Bauwerke"
