9d9b992d8de8a2875ef6560547ef443543da63ca
Arne Binder <arne.binder@dfki.de>
2025-11-03 15:47:53 +0100

    minor

--- src/kibad_llm/schema/types.py
+++ src/kibad_llm/schema/types.py
@@ -46,7 +46,7 @@ class LocationFederalStateEnum(str, Enum):
 class EcosystemTypeCategoryEnum(str, Enum):
     I_MEERE_UND_KUESTEN = "I Biotoptypengruppen der Meere und Küsten"
     II_BINNENGEWAESSER = "II Biotoptypengruppen der Binnengewässer"
-    III_BINNENLAND_ = "III Terrestrische und semiterrestrische Biotoptypengruppen des Binnenlandes"
+    III_BINNENLAND = "III Terrestrische und semiterrestrische Biotoptypengruppen des Binnenlandes"
     IV_TECHNISCHE = "IV Technische Biotoptypengruppen"
     V_ALPEN = "V Biotoptypengruppen mit Schwerpunkt in den Alpen"
     VI_WEITERE = "VI Weitere"
