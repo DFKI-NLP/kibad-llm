4373a982c16e02cfd67c01f1f64ee335e1073e44
Theoreticallyhugo <me@theoreticallyhugo.com>
2025-11-07 11:00:14 +0100

    improve documentation on run_with_llm

--- run_with_llm.sh
+++ run_with_llm.sh
@@ -2,7 +2,7 @@
 
 # default options that can be overridden by the use of flags
 # random port per default to prevent script interference.
-PORT=$(shuf -i 10000-60000 -n 1)  # only ports forwarded through vpn
+export PORT=$(shuf -i 10000-60000 -n 1)  # only ports forwarded through vpn
 PARTITION="RTXA6000-SLT"
 TIME=0-01:00:00
 
@@ -14,19 +14,19 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
     --------------------------------------------------------------------------------
     It is recommended to pass flag values wrapped in quotes like '$0 ... -v \"model/id --trust-remote-code\" ...'
 
-    -v / --vllm         Arguments with which to run vLLM.
-    -po / --port        Port to run vLLM on.                    default=$PORT
-    -pa / --partition   Partition to run vLLM on.               default=$PARTITION
-    -t / --time         Max slurm jub runtime.                  default=$TIME
-    -u / --uv           uv args for the python script to run.
+    -v | --vllm         Arguments with which to run vLLM.
+    -po | --port        Port to run vLLM on.                    default=random(10000-60000)
+    -pa | --partition   Partition to run vLLM on.               default=$PARTITION
+    -t | --time         Max slurm jub runtime.                  default=$TIME
+    -u | --uv           uv args for the python script to run.
     "
     exit
     ;;
   -v | --vllm )
-    shift; VLLM_ARGS=$1
+    shift; export VLLM_ARGS=$1
     ;;
   -po | --port )
-    shift; PORT=$1
+    shift; export PORT=$1
     ;;
   -pa | --partition )
     shift; PARTITION=$1
@@ -35,13 +35,14 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
     shift; TIME=$1
     ;;
   -u | --uv )
-    shift; UV_ARGS=$1
+    shift; export UV_ARGS=$1
     ;;
 esac; shift; done
 if [[ "$1" == '--' ]]; then shift; fi
 
 export HF_HOME="/netscratch/$USER/.cache/hf"
 
+# use uuid to prevent job collision
 UUID="$(uuidgen)"
 JOB_NAME="kiba-d_$UUID"
 
@@ -56,6 +57,28 @@ echo "============================================="
 
 export VLLM_CONFIGURE_LOGGING=0
 
+job(){
+    uvx --cache-dir /netscratch/$USER/cache/uv \
+        vllm serve "$VLLM_ARGS" \
+            --download-dir=/ds/models/llms/cache \
+            --port=$PORT&
+
+    # !the name LLM_API_BASE is important as the extractors hydra config looks for it!
+    export LLM_API_BASE="http://localhost:${PORT}/v1"
+
+    echo ">>> WAITING FOR VLLM STARTUP"
+    until curl --output /dev/null --silent --fail "${LLM_API_BASE}/models"; do
+        sleep 5
+    done
+    echo ">>> VLLM HAS STARTED"
+
+    echo ">>> START UV: uv run $UV_ARGS"
+    uv run $UV_ARGS
+    echo ">>> DONE WITH UV"
+}
+
+export -f job
+
 srun --partition=$PARTITION \
      --job-name="$JOB_NAME" \
      --nodes=1 \
@@ -65,27 +88,4 @@ srun --partition=$PARTITION \
      --mem-per-cpu=4G \
      --oversubscribe \
      --time=$TIME \
-     uvx --cache-dir /netscratch/$USER/cache/uv \
-         vllm serve "$VLLM_ARGS" \
-             --download-dir=/ds/models/llms/cache \
-             --port=$PORT&
-
-SERVER="$(squeue --name="$JOB_NAME" -O NODELIST -h | awk '{$1=$1};1')"
-# !the name LLM_API_BASE is important as the extractors hydra config looks for it!
-export LLM_API_BASE="http://${SERVER}.kl.dfki.de:${PORT}/v1"
-
-echo ">>> WAITING FOR VLLM STARTUP"
-until curl --output /dev/null --silent --fail "${LLM_API_BASE}/models"; do
-    printf '.'
-    sleep 5
-    SERVER="$(squeue --name="$JOB_NAME" -O NODELIST -h | awk '{$1=$1};1')"
-    export LLM_API_BASE="http://${SERVER}.kl.dfki.de:${PORT}/v1"
-done
-echo ">>> VLLM HAS STARTED"
-
-echo ">>> START UV: uv run $UV_ARGS"
-uv run $UV_ARGS
-echo ">>> DONE WITH UV"
-
-# the job needs to be killed explicitly in some cases.
-scancel --name=$JOB_NAME
+     bash -c job
