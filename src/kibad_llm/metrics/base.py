033126ac699f2c596f7bdc5ccb439434202bb092
Arne Binder <arne.binder@dfki.de>
2025-11-08 22:07:48 +0100

    implement ignore_subfields

--- src/kibad_llm/metrics/base.py
+++ src/kibad_llm/metrics/base.py
@@ -3,9 +3,10 @@ from typing import Any
 from kibad_llm.metric import Metric
 
 
-def _convert_dict_to_tuple(d: dict) -> tuple:
+def _convert_dict_to_tuple(d: dict, ignore_keys: list | None = None) -> tuple:
     """Convert a dict to a sorted tuple of its items. Removes None values."""
-    return tuple(sorted((k, v) for k, v in d.items() if v is not None))
+    _ignore_keys = ignore_keys or []
+    return tuple(sorted((k, v) for k, v in d.items() if v is not None and k not in _ignore_keys))
 
 
 class MetricWithPrepareEntryAsSet(Metric):
@@ -13,10 +14,17 @@ class MetricWithPrepareEntryAsSet(Metric):
 
     Args:
         field: Optional; If provided, the field to extract from a dict entry.
+        ignore_subfields: Optional; A dict mapping field names to lists of subfield names to ignore
+            when converting dicts to tuples.
     """
 
-    def __init__(self, field: str | None = None) -> None:
+    def __init__(
+        self, field: str | None = None, ignore_subfields: dict[str, list] | None = None
+    ) -> None:
         self.field = field
+        self.ignore_subfields = []
+        if ignore_subfields is not None and field is not None:
+            self.ignore_subfields = ignore_subfields.get(field, [])
         super().__init__()
 
     def _prepare_entry_as_set(self, entry: Any) -> set:
@@ -43,11 +51,15 @@ class MetricWithPrepareEntryAsSet(Metric):
             # convert list entries to tuples (sort each by key to ensure consistent ordering)
             # to make dicts hashable for the set
             maybe_tuples = (
-                _convert_dict_to_tuple(e) if isinstance(e, dict) else e
+                (
+                    _convert_dict_to_tuple(e, ignore_keys=self.ignore_subfields)
+                    if isinstance(e, dict)
+                    else e
+                )
                 for e in entry
                 if e is not None
             )
             return set(maybe_tuples)
         if isinstance(entry, dict):
-            return {_convert_dict_to_tuple(entry)}
+            return {_convert_dict_to_tuple(entry, ignore_keys=self.ignore_subfields)}
         return {entry}
