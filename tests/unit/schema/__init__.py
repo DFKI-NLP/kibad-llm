DFKI-NLP/kibad-llm

commit 02de390780cdbada4c968e8304f3839b45c9cb90

add schema tests (#87)
to assert the json schema that is generated from the pydantic model. The
generated schema is the "only" interface: it is used to generate the
prompt and for guided decoding.

tests/unit/schema/test_types.py
@@ -1,0 +1,29 @@
+import json
+
+from pydantic import BaseModel
+import pytest
+
+from kibad_llm.config import PROJ_ROOT
+from kibad_llm.schema.types import (
+    EcosystemStudyFeaturesSimple,
+    EcosystemStudyFeaturesWithoutCompounds,
+)
+from tests.conftest import WRITE_FIXTURE_DATA
+
+MODEL2FIXTURE = {
+    EcosystemStudyFeaturesWithoutCompounds: "ecosystem_study_features_without_compounds.txt",
+    EcosystemStudyFeaturesSimple: "ecosystem_study_features_simple.txt",
+}
+
+
+@pytest.mark.parametrize("model_cls", list(MODEL2FIXTURE))
+def test_type(model_cls: type[BaseModel]):
+    schema = model_cls.model_json_schema()
+    path_expected = PROJ_ROOT / "tests" / "fixtures" / "schema" / MODEL2FIXTURE[model_cls]
+    if WRITE_FIXTURE_DATA:
+        with open(path_expected, "w") as f:
+            json.dump(schema, f, indent=2, ensure_ascii=False)
+
+    with open(path_expected) as f:
+        expected = json.load(f)
+    assert schema == expected

commit a3ecefb8ff94209fab0b1fb5d091f020c79a7b45

refactor schema tests (#89)
details:
- move schema description fixture data into `schema_description`
- create fixture file names from class names
- fix schema fixture file name extension: they should be `json`, not
`txt` (regression from #87)
- dynamically get all pydantic models from `kibad_llm.schema.types`
(exclude `Enum`s): this ensures that there is a "test" (i.e., just the
fixture data) for any new pydantic model (*)

*) You can generate the fixture data automatically by temporarily
setting `WRITE_FIXTURE_DATA = True` in `conftest.py` and then run the
respective test.

cc @harbecke

tests/unit/schema/__init__.py
@@ -1,0 +1,22 @@
+from enum import Enum
+import re
+
+from pydantic import BaseModel
+
+from kibad_llm.schema import types
+
+
+def camel_case_to_snake_case(name: str) -> str:
+    """Convert CamelCase string to snake_case string."""
+    return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
+
+
+# get all Pydantic models defined in kibad_llm.schema.types, excluding Enums
+ALL_MODELS = [
+    obj
+    for obj in vars(types).values()
+    if isinstance(obj, type)
+    and issubclass(obj, BaseModel)
+    and obj is not BaseModel
+    and not issubclass(obj, Enum)
+]

tests/unit/schema/test_types.py
@@ -4,22 +4,15 @@ from pydantic import BaseModel
 import pytest
 
 from kibad_llm.config import PROJ_ROOT
-from kibad_llm.schema.types import (
-    EcosystemStudyFeaturesSimple,
-    EcosystemStudyFeaturesWithoutCompounds,
-)
 from tests.conftest import WRITE_FIXTURE_DATA
-
-MODEL2FIXTURE = {
-    EcosystemStudyFeaturesWithoutCompounds: "ecosystem_study_features_without_compounds.txt",
-    EcosystemStudyFeaturesSimple: "ecosystem_study_features_simple.txt",
-}
+from tests.unit.schema import ALL_MODELS, camel_case_to_snake_case
 
 
-@pytest.mark.parametrize("model_cls", list(MODEL2FIXTURE))
+@pytest.mark.parametrize("model_cls", list(ALL_MODELS))
 def test_type(model_cls: type[BaseModel]):
     schema = model_cls.model_json_schema(by_alias=False)
-    path_expected = PROJ_ROOT / "tests" / "fixtures" / "schema" / MODEL2FIXTURE[model_cls]
+    fixture_fn = f"{camel_case_to_snake_case(model_cls.__name__)}.json"
+    path_expected = PROJ_ROOT / "tests" / "fixtures" / "schema" / fixture_fn
     if WRITE_FIXTURE_DATA:
         with open(path_expected, "w") as f:
             json.dump(schema, f, indent=2, ensure_ascii=False)

tests/unit/schema/test_utils.py
@@ -2,23 +2,16 @@ from pydantic import BaseModel
 import pytest
 
 from kibad_llm.config import PROJ_ROOT
-from kibad_llm.schema.types import (
-    EcosystemStudyFeaturesSimple,
-    EcosystemStudyFeaturesWithoutCompounds,
-)
 from kibad_llm.schema.utils import build_schema_description
 from tests.conftest import WRITE_FIXTURE_DATA
-
-MODEL2FIXTURE = {
-    EcosystemStudyFeaturesWithoutCompounds: "description_ecosystem_study_features_without_compounds.txt",
-    EcosystemStudyFeaturesSimple: "description_ecosystem_study_features_simple.txt",
-}
+from tests.unit.schema import ALL_MODELS, camel_case_to_snake_case
 
 
-@pytest.mark.parametrize("model_cls", list(MODEL2FIXTURE))
+@pytest.mark.parametrize("model_cls", list(ALL_MODELS))
 def test_build_schema_description(model_cls: type[BaseModel]):
     description = build_schema_description(model_cls.model_json_schema(by_alias=False))
-    path_expected = PROJ_ROOT / "tests" / "fixtures" / "schema" / MODEL2FIXTURE[model_cls]
+    fixture_fn = f"{camel_case_to_snake_case(model_cls.__name__)}.txt"
+    path_expected = PROJ_ROOT / "tests" / "fixtures" / "schema_description" / fixture_fn
     if WRITE_FIXTURE_DATA:
         with open(path_expected, "w") as f:
             f.write(description)