06c0549acf2f7513028e9296c8de5281b2e311a3
Arne Binder <arne.binder@dfki.de>
2025-11-11 16:12:40 +0100

    move schema description ficture data into schema_description

--- tests/unit/schema/test_utils.py
+++ tests/unit/schema/test_utils.py
@@ -10,15 +10,17 @@ from kibad_llm.schema.utils import build_schema_description
 from tests.conftest import WRITE_FIXTURE_DATA
 
 MODEL2FIXTURE = {
-    EcosystemStudyFeaturesWithoutCompounds: "description_ecosystem_study_features_without_compounds.txt",
-    EcosystemStudyFeaturesSimple: "description_ecosystem_study_features_simple.txt",
+    EcosystemStudyFeaturesWithoutCompounds: "ecosystem_study_features_without_compounds.txt",
+    EcosystemStudyFeaturesSimple: "ecosystem_study_features_simple.txt",
 }
 
 
 @pytest.mark.parametrize("model_cls", list(MODEL2FIXTURE))
 def test_build_schema_description(model_cls: type[BaseModel]):
     description = build_schema_description(model_cls.model_json_schema(by_alias=False))
-    path_expected = PROJ_ROOT / "tests" / "fixtures" / "schema" / MODEL2FIXTURE[model_cls]
+    path_expected = (
+        PROJ_ROOT / "tests" / "fixtures" / "schema_description" / MODEL2FIXTURE[model_cls]
+    )
     if WRITE_FIXTURE_DATA:
         with open(path_expected, "w") as f:
             f.write(description)
