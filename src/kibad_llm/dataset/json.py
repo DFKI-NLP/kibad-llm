8e3c25681001c258f579de184baff11a29b3914d
Arne Binder <arne.binder@dfki.de>
2025-11-26 20:11:01 +0100

    rename load_and_preprocess to read_and_preprocess and improve docstring

--- src/kibad_llm/dataset/json.py
+++ src/kibad_llm/dataset/json.py
@@ -5,19 +5,26 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def load_and_preprocess(
+def read_and_preprocess(
     file: str,
-    id_key: str,
+    id_key: str | None = None,
+    encoding: str = "utf-8",
     process_id: Callable | None = None,
     preprocess: Callable | None = None,
+    **kwargs,
 ) -> dict[Hashable, dict]:
-    """Load a dataset from a JSON file and apply optional preprocessing.
+    """Read a dataset from a JSON (lines) file and apply optional preprocessing.
+    If `id_key` is provided, the JSON file is assumed to be in JSON lines format,
+    and the value corresponding to `id_key` is used as the unique identifier for
+    each entry. If `id_key` is None, the entire JSON file is loaded as a single object.
 
     Args:
         file: Path to the JSON or JSON lines file.
         id_key: Key in the JSON objects to use as the unique identifier.
+        encoding: File encoding to use when reading the file.
         process_id: Optional function to process IDs.
         preprocess: Optional preprocessing function to apply to the dataset.
+        **kwargs: Additional keyword arguments to pass to `json.load` or `json.loads`.
     Returns:
         The loaded and preprocessed Dataset.
     """
@@ -25,14 +32,18 @@ def load_and_preprocess(
     process_id = process_id or (lambda x: x)
 
     logger.info(f"Loading dataset from JSON file: {file} ...")
-    dataset = dict()
-    with open(file, encoding="utf-8") as f:
-        for line in f:
-            if line.strip():
-                line_dict = json.loads(line)
-                key = process_id(line_dict[id_key])
-                del line_dict[id_key]
-                dataset[key] = line_dict
+    if id_key is None:
+        with open(file, encoding=encoding) as f:
+            dataset = json.load(f, **kwargs)
+    else:
+        dataset = dict()
+        with open(file, encoding=encoding) as f:
+            for line in f:
+                if line.strip():
+                    line_dict = json.loads(line, **kwargs)
+                    key = process_id(line_dict[id_key])
+                    del line_dict[id_key]
+                    dataset[key] = line_dict
 
     if preprocess is not None:
         logger.info(f"Apply preprocessing function to dataset: {preprocess} ...")
