9d661c07cd18507d1b534d6dccff9e2e19d51789
Arne Binder <arne.binder@dfki.de>
2025-11-27 01:32:05 +0100

    fix docstring

--- src/kibad_llm/dataset/utils.py
+++ src/kibad_llm/dataset/utils.py
@@ -6,8 +6,7 @@ def merge_references_into_predictions(
     references: dict,
 ) -> dict[Hashable, dict[str, dict]]:
     """Create a new Dataset with entries "prediction" and "reference" by merging references
-    into predictions based on matching IDs. If no matching reference is found for a prediction,
-    the "reference" field will be an empty dict.
+    into predictions based on matching IDs.
 
     Args:
         predictions: Dataset containing prediction entries.
