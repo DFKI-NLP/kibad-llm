5d3243c6ce92be7bf9609ffd06341dde0294d61e
ArneBinder <ArneBinder@users.noreply.github.com>
2025-12-02 13:27:31 +0100

    Add organism trend reference data (#150)
    This implements #145. The data is converted into the same format as what
    extraction with `EcosystemStudyOrganismTrends` from #135 will produce.
    
    details:
    - add "data/external/organism_trends/Weighted Vote Count Wald Literatur
    - Sheet1.csv" based on [this google
    table](https://docs.google.com/spreadsheets/d/14d92Xl3AZhXKNoPpT9he9C0Adjb6oyR6FxO2GD-asn0/edit?gid=0#gid=0)
    (taken from
    [here](https://github.com/DFKI-NLP/kibad-llm/issues/139#issuecomment-3580607029))
     - add a dataset config `organismus_trends_forest.yaml`
    - add parameter `allow_missing_references` to
    `merge_references_into_predictions` and set it for
    `organismus_trends_forest` since the reference data does contain only
    positive instance
    - add note about the new dataset to "Evaluation" section of the
    README.md
    - also rename rename `faktencheck_predictions_with_references` dataset
    config to `faktencheck`

--- README.md
+++ README.md
@@ -134,7 +134,9 @@ uv run -m kibad_llm.evaluate \
 dataset.predictions.file=path/to/predictions.jsonl
 ```
 
-This uses uses `data/interim/faktencheck-db/faktencheck-db-converted_2025-11-05.jsonl` as default reference data and calculates micro averaged precision, recall and F1-score for all fields in the Faktencheck database (i.e., `metric=f1_micro`, see [configs/metric/f1_micro.yaml](./configs/metric/f1_micro.yaml) for details). See [configs/metric](./configs/metric) for other available metrics.
+Per default, this uses `dataset=faktencheck` with `data/interim/faktencheck-db/faktencheck-db-converted_2025-11-05.jsonl` as reference data and calculates micro averaged precision, recall and F1-score for all fields in the Faktencheck database (i.e., `metric=f1_micro`, see [configs/metric/f1_micro.yaml](./configs/metric/f1_micro.yaml) for details). See [configs/metric](./configs/metric) for other available metrics.
+
+To evaluate against a different dataset, set the `dataset` parameter. For instance, to evaluate organism trends for the forest habitat, use `dataset=organism_trends_forest`. See [configs/dataset](./configs/dataset) for available datasets.
 
 See [configs/evaluate.yaml](./configs/evaluate.yaml) for further information and options.
 
---
+++ src/kibad_llm/dataset/csv.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+def remove_nan_from_dict(d: dict) -> dict:
+    """Remove keys with NaN values from a dictionary."""
+    return {k: v for k, v in d.items() if not pd.isna(v)}
+
+
+def read_organism_trends(
+    file: str,
+    pdf_id_column: str = "Key",
+    columns: list[str] | None = None,
+    remove_nan: bool = True,
+) -> dict[str, dict[str, list]]:
+    """Read organism trends from a CSV file. There are multiple trends per pdf ID,
+    so they are grouped into lists.
+
+    Args:
+        file: Path to the CSV file.
+        pdf_id_column: Name of the column containing the pdf IDs.
+        remove_nan: Whether to remove NaN values from the dictionaries.
+        columns: Optional list of columns to read from the CSV file. If not provided,
+                 all columns are read.
+    Returns:
+        A dictionary mapping pdf IDs to their organism trends each represented as a list of dictionaries.
+    """
+    if columns is not None and pdf_id_column not in columns:
+        columns = [pdf_id_column] + columns
+    df = pd.read_csv(file, usecols=columns)
+
+    # Group by pdf_id_column and convert each group to a list of dictionaries
+    result: dict[str, dict[str, list]] = {}
+    for pdf_id, group in df.groupby(pdf_id_column):
+        # remove the pdf_id_column from the group
+        group = group.drop(columns=[pdf_id_column])
+        group_dicts = group.to_dict("records")
+        if remove_nan:
+            group_dicts = [remove_nan_from_dict(d) for d in group_dicts]
+        result[str(pdf_id)] = {"organism_trends": group_dicts}
+
+    return result
