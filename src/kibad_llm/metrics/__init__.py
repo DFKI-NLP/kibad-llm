6121cd9b461ff1959fb2d722f9476253a6c8e597
Arne Binder <arne.binder@dfki.de>
2025-12-10 12:34:02 +0100

    implement ErrorCollector

--- src/kibad_llm/metrics/__init__.py
+++ src/kibad_llm/metrics/__init__.py
@@ -1,3 +1,4 @@
 from .collection import MetricCollection
 from .confusion_matrix import ConfusionMatrix
 from .f1 import F1MicroMultipleFieldsMetric, F1MicroSingleFieldMetric
+from .statistics import ErrorCollector
---
+++ src/kibad_llm/metrics/statistics.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+import logging
+from typing import Any
+
+from kibad_llm.metric import Metric
+
+logger = logging.getLogger(__name__)
+
+
+class ErrorCollector(Metric):
+
+    def __init__(self, show_errors: bool = True) -> None:
+        self.show_errors = show_errors
+        self.reset()
+
+    def reset(self) -> None:
+        self.state: list[str] = []
+
+    def _update(self, prediction: dict[str, Any], reference: dict[str, Any]) -> None:
+        errors_with_none = []
+        if "error" in prediction:
+            errors_with_none.append(prediction["error"])
+        elif "error_list" in prediction and isinstance(prediction["error_list"], list):
+            errors_with_none.extend(prediction["error_list"])
+
+        # separate None values
+        errors_none = ["no_error" for e in errors_with_none if e is None]
+        errors = [e for e in errors_with_none if e is not None]
+        if self.show_errors:
+            for error in errors:
+                logger.info(f"Collected error: {error}")
+        self.state.extend(errors)
+        self.state.extend(errors_none)
+
+    def _compute(self) -> dict[str, Any]:
+
+        # group errors by message and count occurrences
+        errors_grouped = defaultdict(list)
+        for error in self.state:
+            err_parts = str(error).split(": ", 1)
+            errors_grouped[err_parts[0]].append(error)
+
+        # just return counts for now
+        counts = {k: len(v) for k, v in errors_grouped.items()}
+        return counts
