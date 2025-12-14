c0a4fa543df80c8fae7b819bc173c0c91ea2181e
Arne Binder <arne.binder@dfki.de>
2025-12-14 21:42:09 +0100

    remove models/openai_like_vllm.py

--- src/kibad_llm/models/openai_like_vllm.py
+++
@@ -1,7 +0,0 @@
-from llama_index.llms.openai_like import OpenAILike
-
-
-class OpenAILikeVllm(OpenAILike):
-    """Simple wrapper around OpenAI-like LLMs to indicate vLLM usage in extractors.extract_from_text"""
-
-    pass
