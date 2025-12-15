d68f41be76a148a9b0e6dbbbacdfac173195cdec
Leonhard Hennig <leonhard.hennig@dfki.de>
2025-12-15 12:50:34 +0100

    reformatted ground truth content

--- tests/unit/hydra_callbacks/test_save_job_return_value.py
+++ tests/unit/hydra_callbacks/test_save_job_return_value.py
@@ -85,7 +85,9 @@ class TestSaveJobReturnValueCallback:
         elif extension == "md":
             content = file_name.read_text()
             assert content == (
-                "|    |   accuracy |   loss |\n|---:|-----------:|-------:|\n|  0 |       0.95 |   0.05 |"
+                "|    |   accuracy |   loss |\n"
+                "|---:|-----------:|-------:|\n"
+                "|  0 |       0.95 |   0.05 |"
             )
         else:
             pytest.fail(f"Unsupported extension: {extension}")
