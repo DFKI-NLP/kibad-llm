8a03937053e3c80cbd7cad74584c4f61148fbc73
Theoreticallyhugo <me@theoreticallyhugo.com>
2026-01-13 14:05:23 +0100

    not crashing anymore

--- src/kibad_llm/extractors/__init__.py
+++ src/kibad_llm/extractors/__init__.py
@@ -1,5 +1,5 @@
 from .base import extract_from_text, extract_from_text_lenient
+from .chunking import ChunkingExtractor
 from .conditional import ConditionalUnionExtractor
 from .repeat import RepeatingExtractor
 from .union import UnionExtractor
-from .chunking import ChunkingExtractor
