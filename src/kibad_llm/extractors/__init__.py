f643291238d42723ab53cfb1f351e3b84c5c822d
Arne Binder <arne.binder@dfki.de>
2026-02-10 18:30:46 +0100

    export EnsembleExtractor

--- src/kibad_llm/extractors/__init__.py
+++ src/kibad_llm/extractors/__init__.py
@@ -1,4 +1,5 @@
 from .base import extract_from_text, extract_from_text_lenient
 from .conditional import ConditionalUnionExtractor
+from .ensemble import EnsembleExtractor
 from .repeat import RepeatingExtractor
 from .union import UnionExtractor
