b525d07e4a0048a2c65d60bb423eeb74c35f2acf
Theoreticallyhugo <me@theoreticallyhugo.com>
2026-02-11 14:44:43 +0100

    rename chunking helpers to utils

--- src/kibad_llm/extractors/chunking_helpers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tokenizers import RegexTokenizer
-
-# from .tokenizers import tokenizer as tokenizer_lib
-# from .prompting import ContextAwarePromptBuilder
-# from .core import make_batches_of_textchunk
