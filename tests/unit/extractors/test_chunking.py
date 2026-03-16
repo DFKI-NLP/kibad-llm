9a05d5e3bcae9bd3153acb1e7314e47d85565337
Arne Binder <arne.binder@dfki.de>
2026-03-16 16:35:13 +0100

    add test_stride with minimal input document

--- tests/unit/extractors/test_chunking.py
+++ tests/unit/extractors/test_chunking.py
@@ -1,3 +1,4 @@
+from time import time
 from multiprocessing.context import TimeoutError
 from pathlib import Path
 
@@ -5,8 +6,11 @@ import pytest
 
 from kibad_llm.extractors.chunking import _document_chunk_iterator
 from kibad_llm.extractors.chunking_utils import RegexTokenizer
+from kibad_llm.extractors.chunking_utils.core import TextChunk
 from kibad_llm.preprocessing import read_pdf_as_markdown_via_pymupdf4llm
+from tests import FIXTURE_DATA_ROOT
 
+FIXTURE_DATA = FIXTURE_DATA_ROOT / "chunking"
 
 def test_pdf_as_md_bmten2fg() -> None:
     """currently pymupdf fails to convert BMTEN2FG.pdf properly.
@@ -36,3 +40,20 @@ def test_chunking_timeout() -> None:
             1000,
             10,
         )
+
+
+def test_stride() -> None:
+    """x.md is a very simplified version of BMTEN2FG.md with just 233 characters.
+    It should not time out.
+    """
+
+    document_from_md = (FIXTURE_DATA / "x.md").read_text()
+
+    chunks: tuple[TextChunk, ...] = _document_chunk_iterator(
+        document_from_md,
+        200,
+        None,
+        10,
+        10,
+    )
+    # TODO: assert content of chunks
