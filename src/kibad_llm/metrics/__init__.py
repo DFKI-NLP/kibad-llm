816cb9ef858b9b56bc49e5da5772737bbf2e95fe
Arne Binder <arne.binder@dfki.de>
2026-05-15 22:18:50 +0200

    export TpFpFnCollector

--- src/kibad_llm/metrics/__init__.py
+++ src/kibad_llm/metrics/__init__.py
@@ -2,3 +2,4 @@ from .collection import MetricCollection
 from .confusion_matrix import ConfusionMatrix
 from .f1 import F1MicroMultipleFieldsMetric, F1MicroSingleFieldMetric
 from .statistics import ErrorCollector
+from .tpfpfn import TpFpFnCollector
